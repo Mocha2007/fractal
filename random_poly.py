2ca4709af3e7188df484ae805f5abf8b1894866e
mocha <3129210+Mocha2007@users.noreply.github.com>
2022-05-03 23:35:45 -0400

    Update random_poly.py

--- random_poly.py
+++ random_poly.py
@@ -2,17 +2,16 @@ import pygame
 from random import randint, random
 from common import newton
 
-black = 0, 0, 0
-red = 255, 0, 0
+white = 255, 255, 255
 
 size = 1024, 1024
 width, height = size
 
-r_min = -1
-r_max = 1
-i_min = -1
-i_max = 1
-degree = 20 # random quadratics
+r_min = 0
+r_max = 2
+i_min = 0
+i_max = 2
+degree = 24 # random quadratics
 max_coeff_mag = 1
 
 graph_width = r_max - r_min
@@ -32,33 +31,34 @@ def map_to_range(start: float, end: float, fraction: float) -> float:
 
 
 def get_coords_from_z(z: complex) -> tuple[int, int]:
-	x = map_to_range(0, width, z.real / (2*graph_width) + 1/2)
-	y = map_to_range(height, 0, z.imag / (2*graph_height) + 1/2)
+	x = map_to_range(0, width, z.real / graph_width)
+	y = map_to_range(height, 0, z.imag / graph_height)
 	return int(x), int(y)
 
 
 def heat(coords: tuple[int, int]) -> None:
-	current_color = screen.get_at(coords)
-	current_color.r += 10
-	current_color.g += 10
-	current_color.b += 10
-	screen.set_at(coords, current_color)
+	screen.set_at(coords, white)
+
+
+def fourfold_symmetry(z: complex) -> None:
+	_z = abs(z.real) + 1j*abs(z.imag)
+	coords = get_coords_from_z(_z)
+	try:
+		heat(coords)
+	except (IndexError, OverflowError, ValueError):
+		pass
 
 
 def plotting():
 	while 1:
 		# generate random polynomial
-		c = [randint(-max_coeff_mag, max_coeff_mag) for _ in range(degree+1)]
+		c = [(-1)**randint(0, 1) for _ in range(degree+1)]
 		f = lambda x: sum(c[i]*x**i for i in range(degree+1))
 		# find ONE root of it
 		initial_guess = 1j**(4*random())
 		r = newton(f, initial_guess)[-1]
 		# disp
-		coords = get_coords_from_z(r)
-		try:
-			heat(coords)
-		except (IndexError, OverflowError, ValueError):
-			continue
+		fourfold_symmetry(r)
 		refresh()
 		# check if exiting
 		for event in pygame.event.get():
