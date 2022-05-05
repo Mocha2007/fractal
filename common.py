80cb6b6bf092991355236f56a7e015b56a18d0ce
mocha <3129210+Mocha2007@users.noreply.github.com>
2022-05-04 22:40:37 -0400

    gamma, zeta, bernoulli

--- common.py
+++ common.py
@@ -1,6 +1,7 @@
 from colorsys import hsv_to_rgb
-from math import atan2, isnan, pi
+from math import atan2, comb, isnan, pi
 
+e2 = 0.57721566490153286 # Euler–Mascheroni constant
 inf = float('inf')
 nan = float('nan')
 
@@ -8,12 +9,39 @@ color_convergence = 2
 iterations = 20
 tolerance = 1e-6
 
+_bernoulli_cache = [1, -0.5]
+def bernoulli(n: int) -> float:
+	if len(_bernoulli_cache) < n: # need lower ones to be filled for cache
+		bernoulli(n-1)
+	if len(_bernoulli_cache) == n:
+		if n % 2:
+			_bernoulli_cache.append(0)
+		else:
+			# https://en.wikipedia.org/wiki/Bernoulli_number#Efficient_computation_of_Bernoulli_numbers
+			s = 0
+			for k in range(n):
+				for v in range(k):
+					s += (-1)**v * comb(k, v) * v**n / (k+1)
+			_bernoulli_cache.append(s)
+	return _bernoulli_cache[n] 
+
 def derivative(f, x: float, n: int=1) -> float:
 	# assert 0 < n and type(n) == int
 	n -= 1
 	if n:
 		return derivative(lambda a: derivative(f, a), x, n)
 	return (f(x+tolerance) - f(x)) / tolerance
+
+def gamma(z: complex) -> complex:
+	"""Weirstass's definition; converges faster than Euler's definition; 0.5% Error @ i"""
+	from cmath import exp
+	n, g_new, g_old = 0, 1, 0
+	while g_new != g_old and n < 100:
+		# mag 2.6208191893594548e-05 change after 100 steps with z = 1j
+		n += 1
+		g_old = g_new
+		g_new *= exp(z/n) / (1 + z/n)
+	return exp(-e2*z) * g_new / z
 
 def get_filename(raw_string: str) -> str:
 	# invalid chars:
@@ -83,3 +111,21 @@ def newton_prime(z_: complex) -> list[complex]:
 			break
 		z -= c
 	return zlist
+
+_zeta_g_n_cache = [e2]
+def zeta(z: complex) -> complex:
+	# https://en.wikipedia.org/wiki/Riemann_zeta_function#Laurent_series
+	if z == 1:
+		return inf
+	from math import factorial, log
+	max_iter = 10
+	s = 1/(z-1)
+	for n in range(max_iter):
+		if len(_zeta_g_n_cache) <= n:
+			_zeta_g_n_cache.append(
+				sum(log(k)**n / k for k in range(1, max_iter+1))
+				- log(max_iter)**(n+1) / (n+1)
+			)
+		g_n = _zeta_g_n_cache[n]
+		s += g_n / factorial(n) * (1-z)**n
+	return s
